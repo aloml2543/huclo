4349b72c594a392d07a09e4654954be23cfaef5a
aloml2543 <55018554+aloml2543@users.noreply.github.com>
2023-03-29 18:50:06 +0900

    Add files via upload

---
+++ src/main/java/Ksol/mapManage/MapManageApplication.java
@@ -0,0 +1,13 @@
+package Ksol.mapManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapManageApplication.class, args);
+	}
+
+}
